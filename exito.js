ed1cef730fe6ea5b2e93feff030f687c90e2fc10
gisele bagialemani <gisebag@gmail.com>
2022-11-22 17:26:37 -0300

    form

---
+++ exito.js
@@ -0,0 +1,3 @@
+setTimeout(function() {
+    window.location.replace("form.html");
+}, 2000);
---
+++ form.php
@@ -0,0 +1,22 @@
+<?php
+
+$nombre =  $_POST['name'];
+$email =  $_POST['email'];
+$colaborar =  $_POST['talk-type'];
+$redes =  $_POST['redesSociales'];
+$quehaces =  $_POST['queHaces'];
+$colaborar =  $_POST['colaborar'];
+$otro =  $_POST['otroComentario'];
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "su correo es " .$email . "\r\n";
+$mensaje .= "Asunto " .$colaborar . "\r\n";
+$mensaje .= "Redes sociales " .$redes . "\r\n";
+$mensaje .= "que haces " .$quehaces . "\r\n";
+$mensaje .= "Colaboraciones " .$colaborar . "\r\n";
+$mensaje .= "otros " .$otro . "\r\n";
+$mensaje .= "Enviado el " .date('d/m/Y',time());
+
+
+
+?>
