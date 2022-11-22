adc1f6996e5e5feab9a5d4582d29c1862898b4aa
gisele bagialemani <gisebag@gmail.com>
2022-11-22 18:06:36 -0300

    prueba

--- form.php
+++ form.php
@@ -17,6 +17,12 @@ $mensaje .= "Colaboraciones " .$colaborar . "\r\n";
 $mensaje .= "otros " .$otro . "\r\n";
 $mensaje .= "Enviado el " .date('d/m/Y',time());
 
+$para = 'gisebag@gmail.com';
+$asunto = 'Contacto sitio web';
 
+//funcion mail (obligacion)
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header('Location:exito.html')
 
 ?>
