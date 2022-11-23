1de5d3ed2dc5a89190fe88c28e5cd181f8da0f4a
gisele bagialemani <gisebag@gmail.com>
2022-11-22 21:07:47 -0300

    .

--- form.php
+++ form.php
@@ -2,14 +2,14 @@
 
 $nombre =  $_POST['name'];
 $email =  $_POST['email'];
-$colaborar =  $_POST['talk-type'];
+$colaborar =  $_POST['talk'];
 $redes =  $_POST['redesSociales'];
 $quehaces =  $_POST['queHaces'];
 $colaborar =  $_POST['colaborar'];
 $otro =  $_POST['otroComentario'];
 
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$mensaje .= "su correo es " .$email . "\r\n";
+$mensaje = "su correo es " .$email . "\r\n";
 /* $mensaje .= "Asunto " .$colaborar . "\r\n";
 $mensaje .= "Redes sociales " .$redes . "\r\n";
 $mensaje .= "que haces " .$quehaces . "\r\n";
@@ -17,7 +17,7 @@ $mensaje .= "Colaboraciones " .$colaborar . "\r\n";
 $mensaje .= "otros " .$otro . "\r\n"; */
 $mensaje .= "Enviado el " .date('d/m/Y',time());
 
-$para = 'gisebag@gmail.com';
+$para = 'foxbiscuitdesign@gmail.com';
 $asunto = 'Contacto sitio web';
 
 //funcion mail (obligacion)
