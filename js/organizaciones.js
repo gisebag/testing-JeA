8cb5a3a760934e3488cf46ef27d3ad06a612ec60
Joaquín Carré <joa_carre21@hotmail.com>
2023-02-21 17:47:38 -0300

    Mejorando sección tarjetas

--- js/organizaciones.js
+++ js/organizaciones.js
@@ -1,29 +1,3 @@
-/* TARJETAS */
-//Aplicando Hover
-/* const tarjeta1 = document.getElementsByClassName('tarjeta-organizacion1');
-const tarjeta1Hover = document.getElementsByClassName('tarjeta-organizacion11');
-tarjeta1.addEventListener('mouseover', function () {
-  tarjeta1.classList.add('d-none');
-  tarjeta1Hover.classList.remove('d-none');
-})
-tarjeta1.addEventListener('mouseout', function () {
-  tarjeta1.classList.remove('d-none');
-  tarjeta1Hover.classList.add('d-none');
-})
-
-for (let i = 1; i < 6; i++) {
-  const tarjeta = document.getElementsByClassName(`tarjeta-organizacion${i}`);
-  const tarjetaHover = document.getElementsByClassName(`tarjeta-organizacion${i}${i}`);
-  tarjeta.addEventListener('mouseover', function () {
-    tarjeta.classList.add('d-none');
-    tarjetaHover.classList.remove('d-none');
-  })
-  tarjeta.addEventListener('mouseout', function () {
-    tarjeta.classList.remove('d-none');
-    tarjetaHover.classList.add('d-none');
-  })
-} */
-
 /* CAROUSEL */
 //Haciendo la barra de desplazamiento invisible
 const element = document.getElementsByClassName('elemento');
