dc2c775b760b9a83661451647247c0a95dead9b5
Joaquín Carré <joa_carre21@hotmail.com>
2023-02-21 14:23:56 -0300

    Realizando página 'Organizaciones' sección '¿Que hacemos?'

--- js/organizaciones.js
+++ js/organizaciones.js
@@ -1,19 +1,68 @@
-const images = document.querySelector('.carousel-images');
-const carouselArrowLeft = document.querySelector('.carousel-arrow-left');
-const carouselArrowRight = document.querySelector('.carousel-arrow-right');
+/* TARJETAS */
+//Aplicando Hover
+/* const tarjeta1 = document.getElementsByClassName('tarjeta-organizacion1');
+const tarjeta1Hover = document.getElementsByClassName('tarjeta-organizacion11');
+tarjeta1.addEventListener('mouseover', function () {
+  tarjeta1.classList.add('d-none');
+  tarjeta1Hover.classList.remove('d-none');
+})
+tarjeta1.addEventListener('mouseout', function () {
+  tarjeta1.classList.remove('d-none');
+  tarjeta1Hover.classList.add('d-none');
+})
 
-let currentImageIndex = 0;
+for (let i = 1; i < 6; i++) {
+  const tarjeta = document.getElementsByClassName(`tarjeta-organizacion${i}`);
+  const tarjetaHover = document.getElementsByClassName(`tarjeta-organizacion${i}${i}`);
+  tarjeta.addEventListener('mouseover', function () {
+    tarjeta.classList.add('d-none');
+    tarjetaHover.classList.remove('d-none');
+  })
+  tarjeta.addEventListener('mouseout', function () {
+    tarjeta.classList.remove('d-none');
+    tarjetaHover.classList.add('d-none');
+  })
+} */
 
-carouselArrowLeft.addEventListener('click', () => {
-  if (currentImageIndex > 0) {
-    currentImageIndex--;
-    images.style.transform = `translateX(-${currentImageIndex * 20}%)`;
-  }
-});
+/* CAROUSEL */
+//Haciendo la barra de desplazamiento invisible
+const element = document.getElementsByClassName('elemento');
+const scrollbarWidth = window.getComputedStyle(element[0]).getPropertyValue('scrollbar-width');
 
-carouselArrowRight.addEventListener('click', () => {
-  if (currentImageIndex < 4) {
-    currentImageIndex++;
-    images.style.transform = `translateX(-${currentImageIndex * 20}%)`;
-  }
-});
+//Dando función a las flechas del carousel
+const contenedor = document.getElementsByClassName('carrusel-div')[0];
+const contenido = document.getElementsByClassName('carrusel')[0];
+const flechaIzquierda = document.getElementsByClassName('flecha-izquierda');
+const flechaDerecha = document.getElementsByClassName('flecha-derecha');
+
+contenido.style.width = 90 + '%';
+
+let izquierdaPresionada = false;
+let derechaPresionada = false;
+let scrollInterval;
+
+for (let i = 0; i < flechaIzquierda.length; i++) {
+  flechaIzquierda[i].addEventListener('mouseover', function () {
+    izquierdaPresionada = true;
+    scrollInterval = setInterval(function () {
+      contenido.scrollLeft -= 5; // se disminuye en 5 unidades cada 30ms
+    }, 30);
+  });
+  flechaIzquierda[i].addEventListener('mouseout', function () {
+    izquierdaPresionada = false;
+    clearInterval(scrollInterval);
+  });
+}
+
+for (let i = 0; i < flechaDerecha.length; i++) {
+  flechaDerecha[i].addEventListener('mouseover', function () {
+    derechaPresionada = true;
+    scrollInterval = setInterval(function () {
+      contenido.scrollLeft += 5; // se aumenta en 5 unidades cada 30ms
+    }, 30);
+  });
+  flechaDerecha[i].addEventListener('mouseout', function () {
+    derechaPresionada = false;
+    clearInterval(scrollInterval);
+  });
+}
