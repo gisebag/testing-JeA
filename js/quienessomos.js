69272a384cbc48a895d1a4a8cd26bfd01497c46d
Joaquín Carré <joa_carre21@hotmail.com>
2023-04-19 23:53:09 -0300

    Actualizando página 'Quienes somos' y haciendola responsive - falta jumbotron

--- js/quienessomos.js
+++ js/quienessomos.js
@@ -2,20 +2,32 @@ const btnConocenos = document.getElementById('btnConocenos');
 const textoHistoria = document.getElementById('textoHistoria');
 const divFotosCerrado = document.getElementById('recuadro-fotos-cerrado');
 const divFotosAbierto = document.getElementById('recuadro-fotos-abierto');
-const btnFlecha = document.getElementById('btnFlecha');
+const btnFlechaAbajo = document.getElementById('btnFlechaAbajo');
+const btnFlechaArriba = document.getElementById('btnFlechaArriba');
 
 btnConocenos.addEventListener('click', () => {
-        textoHistoria.innerHTML = '<p>JeA nació en 2020 a raíz de las Jornadas Iberoamericanas de la Educación Intercultural llevadas a cabo por AFS Programas Interculturales. Carolina, de España, junto a Amparo, Delfina y Luna de Argentina coincidieron en el “Panel para Jóvenes: Ciudadanos Globales cambiando sus comunidades” y, tras ver la increíble respuesta de la juventud (más de 300 jóvenes se unieron al encuentro), se empezó a gestar el proyecto como necesidad de un espacio que potencie la conectividad activa entre jóvenes hispano hablantes.</p><br><p>Nos dimos cuenta que muchos jóvenes quieren llevar a cabo iniciativas para hacer una diferencia en sus comunidades y globalmente pero no saben por dónde ni cómo empezar. Queremos orientar su accionar. Y a su vez, muchos otros jóvenes tienen proyectos increíbles que no son lo suficientemente reconocidos. Queremos hacerlos visibles y apoyarlos.</p><br><p><span>Más de la mitad de la población mundial tiene menos de 30 años y no está adecuadamente representada en los sistemas sociales, políticos y económicos.</span></p><br><p>Creemos firmemente que el futuro debe ser moldeado por la juventud con decisiones que reflejen sus necesidades basadas en un intercambio intergeneracional de ideas. Necesitamos crear más oportunidades para participar de forma significativa en la toma de decisiones, para que así podamos tener un mayor control sobre la configuración de nuestras vidas y nuestro futuro,hacer oír nuestras voces, colaborando conjuntamente.</p><br><span>JeA surge de la necesidad de crear un espacio hecho POR y PARA jóvenes. Funcionamos como un “embudo” de información para direccionar tu accionar y generar un IMPACTO.</span> <br>'
-        btnConocenos.classList.add('d-none');
-        divFotosCerrado.classList.add('d-none');
-        divFotosAbierto.classList.remove('d-none');
-        btnFlecha.classList.remove('d-none');
+    textoHistoria.innerHTML = '<p>JeA nació en 2020 a raíz de las Jornadas Iberoamericanas de la Educación Intercultural llevadas a cabo por AFS Programas Interculturales. Carolina, de España, junto a Amparo, Delfina y Luna de Argentina coincidieron en el “Panel para Jóvenes: Ciudadanos Globales cambiando sus comunidades” y, tras ver la increíble respuesta de la juventud (más de 300 jóvenes se unieron al encuentro), se empezó a gestar el proyecto como necesidad de un espacio que potencie la conectividad activa entre jóvenes hispano hablantes.</p><br><p>Nos dimos cuenta que muchos jóvenes quieren llevar a cabo iniciativas para hacer una diferencia en sus comunidades y globalmente pero no saben por dónde ni cómo empezar. Queremos orientar su accionar. Y a su vez, muchos otros jóvenes tienen proyectos increíbles que no son lo suficientemente reconocidos. Queremos hacerlos visibles y apoyarlos.</p><br><p><span>Más de la mitad de la población mundial tiene menos de 30 años y no está adecuadamente representada en los sistemas sociales, políticos y económicos.</span></p><br><p>Creemos firmemente que el futuro debe ser moldeado por la juventud con decisiones que reflejen sus necesidades basadas en un intercambio intergeneracional de ideas. Necesitamos crear más oportunidades para participar de forma significativa en la toma de decisiones, para que así podamos tener un mayor control sobre la configuración de nuestras vidas y nuestro futuro,hacer oír nuestras voces, colaborando conjuntamente.</p><br><span>JeA surge de la necesidad de crear un espacio hecho POR y PARA jóvenes. Funcionamos como un “embudo” de información para direccionar tu accionar y generar un IMPACTO.</span> <br>'
+    btnConocenos.classList.add('d-none');
+    divFotosCerrado.classList.add('d-none');
+    divFotosAbierto.classList.remove('d-none');
+    btnFlechaAbajo.classList.add('d-none');
+    btnFlechaArriba.classList.remove('d-none');
 });
 
-btnFlecha.addEventListener('click', () => {
+btnFlechaAbajo.addEventListener('click', () => {
+    textoHistoria.innerHTML = '<p>JeA nació en 2020 a raíz de las Jornadas Iberoamericanas de la Educación Intercultural llevadas a cabo por AFS Programas Interculturales. Carolina, de España, junto a Amparo, Delfina y Luna de Argentina coincidieron en el “Panel para Jóvenes: Ciudadanos Globales cambiando sus comunidades” y, tras ver la increíble respuesta de la juventud (más de 300 jóvenes se unieron al encuentro), se empezó a gestar el proyecto como necesidad de un espacio que potencie la conectividad activa entre jóvenes hispano hablantes.</p><br><p>Nos dimos cuenta que muchos jóvenes quieren llevar a cabo iniciativas para hacer una diferencia en sus comunidades y globalmente pero no saben por dónde ni cómo empezar. Queremos orientar su accionar. Y a su vez, muchos otros jóvenes tienen proyectos increíbles que no son lo suficientemente reconocidos. Queremos hacerlos visibles y apoyarlos.</p><br><p><span>Más de la mitad de la población mundial tiene menos de 30 años y no está adecuadamente representada en los sistemas sociales, políticos y económicos.</span></p><br><p>Creemos firmemente que el futuro debe ser moldeado por la juventud con decisiones que reflejen sus necesidades basadas en un intercambio intergeneracional de ideas. Necesitamos crear más oportunidades para participar de forma significativa en la toma de decisiones, para que así podamos tener un mayor control sobre la configuración de nuestras vidas y nuestro futuro,hacer oír nuestras voces, colaborando conjuntamente.</p><br><span>JeA surge de la necesidad de crear un espacio hecho POR y PARA jóvenes. Funcionamos como un “embudo” de información para direccionar tu accionar y generar un IMPACTO.</span> <br>'
+    btnConocenos.classList.add('d-none');
+    divFotosCerrado.classList.add('d-none');
+    divFotosAbierto.classList.remove('d-none');
+    btnFlechaAbajo.classList.add('d-none');
+    btnFlechaArriba.classList.remove('d-none');
+});
+
+btnFlechaArriba.addEventListener('click', () => {
     textoHistoria.innerHTML = '<p>JeA nació en 2020 a raíz de las Jornadas Iberoamericanas de la Educación Intercultural llevadas a cabo por AFS Programas Interculturales...</p>'
     btnConocenos.classList.remove('d-none');
     divFotosCerrado.classList.remove('d-none');
     divFotosAbierto.classList.add('d-none');
-    btnFlecha.classList.add('d-none');
+    btnFlechaAbajo.classList.remove('d-none');
+    btnFlechaArriba.classList.add('d-none');
 });
