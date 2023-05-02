53df4c80f885d23cd1f67c536c1bf1fb0ec37365
gisele bagialemani <gisebag@gmail.com>
2023-05-02 00:16:26 -0300

    responsive index falta

---
+++ js/index.js
@@ -0,0 +1,30 @@
+/* CAROUSEL */
+const carrousel = document.querySelector(".carrousel-items");
+
+let maxScrollLeft = carrousel.scrollWidth - carrousel.clientWidth;
+let intervalo = null;
+let step = 0.5;
+const start = () => {
+    intervalo = setInterval(function () {
+        carrousel.scrollLeft = carrousel.scrollLeft + step;
+        if (carrousel.scrollLeft === maxScrollLeft) {
+            step = step * -1;
+        } else if (carrousel.scrollLeft === 0){
+            step = step * -1;
+        }
+    }, 10);
+};
+
+const stop = () =>{
+    clearInterval(intervalo);
+}
+
+carrousel.addEventListener("mouseover", () =>{
+    stop();
+});
+
+carrousel.addEventListener("mouseout", () =>{
+    start();
+});
+
+start();s
