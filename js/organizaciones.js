2dc3ea8257c6f7d6d1e64759f4963751cb5e4927
Joaquín Carré <joa_carre21@hotmail.com>
2023-05-13 23:35:23 -0300

    Ultimando detalles de página Organizaciones

--- js/organizaciones.js
+++ js/organizaciones.js
@@ -1,3 +1,33 @@
+/* BOTONES TARJETAS */
+for (let i = 1; i < 6; i++) {
+  const botonVerMas = document.getElementById(`boton-ver-mas-${i}`);
+  const tarjetaImagen = document.getElementById(`tarjeta-imagen-${i}`);
+  const tarjetaDescripcion = document.getElementById(`tarjeta-descripcion-${i}`);
+  const botonCerrarTarjeta = document.getElementById(`cerrar-tarjeta-${i}`);
+  
+  botonVerMas.addEventListener('click', () => {
+    tarjetaImagen.classList.remove('mostrar');
+    tarjetaImagen.classList.add('ocultar');
+    tarjetaDescripcion.classList.remove('ocultar');
+    tarjetaDescripcion.classList.add('mostrar');
+    botonVerMas.classList.add('d-none');
+    setTimeout(() => {
+      botonCerrarTarjeta.classList.remove('d-none');
+    }, 250);
+  });
+  
+  botonCerrarTarjeta.addEventListener('click', () => {
+    tarjetaImagen.classList.remove('ocultar');
+    tarjetaImagen.classList.add('mostrar');
+    tarjetaDescripcion.classList.remove('mostrar');
+    tarjetaDescripcion.classList.add('ocultar');
+    botonCerrarTarjeta.classList.add('d-none');
+    setTimeout(() => {
+      botonVerMas.classList.remove('d-none');
+    }, 250);
+  });
+}
+
 /* CAROUSEL */
 //Haciendo la barra de desplazamiento invisible
 const element = document.getElementsByClassName('elemento');
