8a20a75fdf6dff5d7929a9cd24d7320f9cebe87b
Joaquín Carré <joa_carre21@hotmail.com>
2023-06-04 16:41:53 -0300

    Subiendo avances Carousel jovenes

--- js/jovenes.js
+++ js/jovenes.js
@@ -1,18 +1,11 @@
-let carousel = new bootstrap.Carousel(document.getElementById('carouselExample'), {
-    interval: false,
-    wrap: false
-});
-
 /* BOTONES TARJETAS */
 for (let i = 1; i < 6; i++) {
     const botonVerMas = document.getElementById(`boton-ver-mas-${i}`);
-    const tarjetaImagen = document.getElementById(`tarjeta-imagen-${i}`);
     const tarjetaDescripcion = document.getElementById(`tarjeta-descripcion-${i}`);
     const botonCerrarTarjeta = document.getElementById(`cerrar-tarjeta-${i}`);
+    const imagenLogo = document.getElementById(`imagen-logo-${i}`)
     
     botonVerMas.addEventListener('click', () => {
-      tarjetaImagen.classList.remove('mostrar');
-      tarjetaImagen.classList.add('ocultar');
       tarjetaDescripcion.classList.remove('ocultar');
       tarjetaDescripcion.classList.add('mostrar');
       botonVerMas.classList.add('d-none');
@@ -22,8 +15,6 @@ for (let i = 1; i < 6; i++) {
     });
     
     botonCerrarTarjeta.addEventListener('click', () => {
-      tarjetaImagen.classList.remove('ocultar');
-      tarjetaImagen.classList.add('mostrar');
       tarjetaDescripcion.classList.remove('mostrar');
       tarjetaDescripcion.classList.add('ocultar');
       botonCerrarTarjeta.classList.add('d-none');
