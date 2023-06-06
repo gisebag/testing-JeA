2ae787194dd8a973c478a0bf4965c36342363063
Joaquín Carré <joa_carre21@hotmail.com>
2023-06-06 00:28:12 -0300

    Creando documento JS para jovenes

---
+++ js/jovenes.js
@@ -0,0 +1,25 @@
+/* BOTONES TARJETAS */
+for (let i = 1; i < 8; i++) {
+  const botonVerMas = document.getElementById(`boton-ver-mas-${i}`);
+  const tarjetaDescripcion = document.getElementById(`tarjeta-descripcion-${i}`);
+  const botonCerrarTarjeta = document.getElementById(`cerrar-tarjeta-${i}`);
+  const imagenLogo = document.getElementById(`imagen-logo-${i}`)
+  
+  botonVerMas.addEventListener('click', () => {
+    tarjetaDescripcion.classList.remove('ocultar');
+    tarjetaDescripcion.classList.add('mostrar');
+    botonVerMas.classList.add('d-none');
+    setTimeout(() => {
+      botonCerrarTarjeta.classList.remove('d-none');
+    }, 250);
+  });
+  
+  botonCerrarTarjeta.addEventListener('click', () => {
+    tarjetaDescripcion.classList.remove('mostrar');
+    tarjetaDescripcion.classList.add('ocultar');
+    botonCerrarTarjeta.classList.add('d-none');
+    setTimeout(() => {
+      botonVerMas.classList.remove('d-none');
+    }, 250);
+  });
+}
