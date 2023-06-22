e6245e6d0d3c35b916dcc4d1f3b615fc48b8ac6e
Joaquín Carré <joa_carre21@hotmail.com>
2023-06-22 20:59:40 -0300

    Actualizando php

--- envio-formulario.php
+++ envio-formulario.php
@@ -9,16 +9,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $asunto = "Nuevo mensaje jovenaccion.com";
   $cuerpo = "Nombre y Apellido: " . $nombre . "\n"
           . "E-mail: " . $email . "\n"
-          . "Teléfono: " . $telefono . "\n"
+          . "Telefono: " . $telefono . "\n"
           . "Mensaje: " . $mensaje;
 
   // Enviar el correo
   $resultado = mail($destinatario, $asunto, $cuerpo);
 
   if ($resultado) {
-    echo "Gracias por completar el formulario. Nos pondremos en contacto contigo pronto.";
+    echo '<div style="background-color: #02503E; color: #000; padding: 10px; text-align: center;">
+    <h4">Gracias por completar el formulario.</h4>
+    <p>Nos pondremos en contacto contigo pronto.</p>
+    <p>Redirigiendo en 3 segundos...</p>
+  </div>';
+    header("refresh:3;url=https://jovenaccion.com/Contacto/contacto.html");
+    exit();
   } else {
-    echo "Hubo un error al enviar el formulario. Por favor, intenta nuevamente.";
+    echo "Hubo un error al enviar el formulario. Por favor, intenta nuevamente. Serás redirigido en 3 segundos...";
+    header("refresh:3;url=https://jovenaccion.com/Contacto/contacto.html");
+    exit();
   }
 }
 ?>
